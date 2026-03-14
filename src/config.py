4e3f170d7fbc033356fd24bdca18c6c5303d5681
tlgb <tlgbkakaka@gmail.com>
2026-03-14 11:21:02 +0700

    chuye n volume ve config het k dat trong main nua

--- src/config.py
+++ src/config.py
@@ -1,5 +1,3 @@
-import os
-
 DB_CONFIG = {
     "host": "localhost",
     "user": "postgres",
@@ -7,3 +5,25 @@ DB_CONFIG = {
     "database": "ecommerce_oltp",
     "port": 5432,
 }
+
+SEED = 42
+
+DATA_VOLUME = {
+    "brand": 20,
+    "category": 10,
+    "seller": 25,
+    "product": 2000,
+    "promotion": 10,
+    "promotion_product": 100
+}
+
+CATEGORY_MAP = {
+    "Electronics": ["Smartphones", "Laptops", "Tablets", "Accessories", "Cameras"],
+    "Fashion": ["Clothing", "Shoes", "Bags", "Jewelry", "Watches"],
+    "Home Appliances": ["Kitchenware", "Washing Machines", "Air Purifiers", "Fridges"],
+    "Beauty": ["Skincare", "Makeup", "Perfume", "Hair Care"],
+    "Sports": ["Gym Gear", "Outdoor Equipment", "Yoga Mats", "Cycling"]
+}
+
+PROMO_NAMES = ["Mega Sale", "Flash Deal", "Black Friday", "Birthday", "Holiday"]
+PROMO_TYPES = ["Seasonal", "Flash Sale", "Member Only", "Voucher Code"]
